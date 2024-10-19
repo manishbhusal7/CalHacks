88c77826b949f8f0af0b6797997260267dd7c798
nima64 <kennethe407@gmail.com>
2024-10-19 10:56:27 -0700

    back end api takes data not presends string

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,14 @@
 import Groq from "groq-sdk";
-import cors from 'cors';
-import express from 'express';
+import cors from "cors";
+import express from "express";
 
-const GROQ_API_KEY = "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"
+const GROQ_API_KEY = "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";
 
 const groq = new Groq({ apiKey: GROQ_API_KEY });
 
-export async function getGroqChatCompletion(messages, model) {  
-  return groq.chat.completions.create({ 
-    messages: [
-      {
-        role: "user",
-        content: "Explain the importance of fast language models",
-      },
-    ],
-    model: "llama3-8b-8192",
-  });
+export async function getGroqChatCompletion(data) {
+  console.log(`Received messages: ${JSON.stringify(data)}`);
+  return groq.chat.completions.create(data);
 }
 
 const app = express();
@@ -24,11 +17,12 @@ const port = 3000;
 
 app.use(express.json());
 
-app.post('/chat', async (req, res) => {
+app.post("/chat", async (req, res) => {
   try {
-    const { messages, model } = req.body;
-    const completion = await getGroqChatCompletion(messages, model);
-      // Print the completion returned by the LLM.
+    console.log(`Received body: ${JSON.stringify(req.body)}`);
+    const { data, headers } = req.body;
+    const completion = await getGroqChatCompletion(data);
+    // Print the completion returned by the LLM.
     console.log(`Received request: ${JSON.stringify(req.body)}`);
     // console.log(`Returning completion: ${JSON.stringify(completion)}`);
     res.json(completion);
--- front-end/src/api.js
+++ front-end/src/api.js
@@ -1,21 +1,25 @@
 import axios from 'axios';
-const fetchApiData = async () => {
+
+const fetchApiData = async (prompt) => {
     try {
       const response = await axios.post('http://localhost:3000/chat', {
         headers: {
           'Content-Type': 'application/json',
         },
         data: {
-          messages: {
+          messages: [{
             role: 'user',
-            content: 'Explain the importance of fast language models',
-          },
-          model: 'llam8',
+            content: prompt,
+          }],
+          model: 'llama3-8b-8192',
         },
       });
-      console.log(response.data);
+    //   console.log(response.data);
+      return response.data;
     } catch (error) {
       console.error('Error making request:', error);
+      throw new Error('Failed to fetch data'); // Throw an error with a descriptive message
     }
-  };
+};
+
 export default fetchApiData;
